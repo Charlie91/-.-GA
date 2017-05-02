4ff6bbc89c16095574f089dae2b07bae970a5466
Charlie91 <aleksand44@yandex.ru>
2017-05-02 19:54:33 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 var url = window.location.href,
 	  phones = document.getElementsByClassName('ct_phone'),
-	  button = document.getElementById('telephone');
+	  button = document.getElementById('telephone'); //захватываем кнопку звонка
 
-	if(~url.indexOf("clid")){ //определяем что пользователь с директа
+	if(~url.indexOf("clid")){ //определяем что пользователь с директа и адвордс
 		var date = new Date;
 		date.setDate(date.getDate() + 10);
-		document.cookie = "source=yaDirect;expires="+ date.toUTCString()+ ";path=/;";
+		document.cookie = "source=yaDirect;expires="+ date.toUTCString()+ ";path=/;"; //отправляем куку
 	}
-	if(getCookie('source') == 'yaDirect'){
+	if(getCookie('source') == 'yaDirect'){ // Если есть кука - меняем номер
 		button.setAttribute('href','tel:+74957683110');
 		phones[0].innerHTML = '<a href="tel:+74957683110">+7 (495) 768-31-10</a>';
 		phones[1].innerHTML = '<a href="tel:+74957683110">+7 (495) 768-31-10</a>';
